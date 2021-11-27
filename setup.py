1fb725994df797c05bc4a019ec599a73d9585cf1
lhwdev <lhwdev6@outlook.com>
2021-11-28 01:18:17 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='hcskr',
+
+    version='1.3.6',
+
+    description='코로나 자가진단 라이브러리 (Automation tool for https://hcs.eduro.go.kr/)',
+    license="GPL-V3",
+    author='LeoK',
+
+    # author_email='support@leok.kr',
+    url='https://github.com/covid-hcs/hcs-python',
+
+    download_url='https://github.com/covid-hcs/hcs-python',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    install_requires=['pycryptodome','requests'],
+
+    packages=find_packages(),
+
+    keywords=['korea','covid','auto','hcs','corona'],
+
+    python_requires='>=3',
+
+
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        #'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
+    ],
+)
