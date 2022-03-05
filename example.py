58ce4a43634cb8401406d511b3ab1ddc5fa1a312
lhwdev <lhwdev6@outlook.com>
2022-03-05 20:33:37 +0900

    Update example.py and __init__.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from hcskr.hcskr import selfcheck
+from hcskr.hcskr import selfcheck, QuickTestResult
 
 name = input("이름을 입력하세요: ")
 birth = input("생년월일을 입력하세요: ")
@@ -6,6 +6,7 @@ level = input("학교종류를 입력하세요(예: 초등학교, 중학교, 고
 region = input("지역을 입력하세요(예: 서울, 경기, 전남....): ")
 school = input("학교이름을 입력하세요(예: 두둥실고): ")
 password = input("비밀번호를 입력하세요: ")
-data = selfcheck(name,birth,region,school,level,password)
+quickTestResult = input("신속항원검사 결과를 입력하세요(none, negative, positive): ")
+data = selfcheck(name,birth,region,school,level,password, quickTestResult=QuickTestResult(quickTestResult))
 
 print(data['message'])
